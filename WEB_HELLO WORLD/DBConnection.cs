383c65d56c713b8d9d268150f85d8f94a82aa059
siyamnd <siyamndmohammad216@gmail.com>
2022-10-08 10:41:18 +0300

    add files

---
+++ WEB_HELLO WORLD/DBConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB_HELLO_WORLD
+{
+    public class DBConnection
+    {
+        public string ConnectionString { 
+            get
+            {
+                return "";
+            }
+                }
+    }
+}
